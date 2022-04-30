a350c76dbd006d886bdac9358a29516337b2290d
Emery Hemingway <ehmry@posteo.net>
2022-04-30 12:52:42 -0500

    [libc++] add support/genode/xlocale.h

---
+++ libcxx/include/support/genode/xlocale.h
@@ -0,0 +1,21 @@
+// -*- C++ -*-
+//===------------------- support/genode/xlocale.h ------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef _LIBCPP_SUPPORT_GENODE_XLOCALE_H
+#define _LIBCPP_SUPPORT_GENODE_XLOCALE_H
+
+#if defined(__GENODE__)
+
+#include <cstdlib>
+#include <cwchar>
+#include <support/xlocale/__strtonum_fallback.h>
+
+#endif // defined(__GENODE__)
+
+#endif // _LIBCPP_SUPPORT_GENODE_XLOCALE_H
